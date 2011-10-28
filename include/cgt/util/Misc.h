62002d4f81134134799e09e6e4bc01553bd0d21d
Bloody.Rabbit <jan.bobek@gmail.com>
2011-10-28 21:58:13 +0200

    cgt/util: Add misc utils.

---
+++ include/cgt/util/Misc.h
@@ -0,0 +1,48 @@
+/**
+ * @file util/Misc.h
+ *
+ * Console Guitar Tuner (CGT)
+ * Copyright (c) 2011 by Bloody.Rabbit
+ *
+ * @author Bloody.Rabbit
+ */
+
+#ifndef __UTIL__MISC_H__INCL__
+#define __UTIL__MISC_H__INCL__
+
+namespace util {
+
+/// Names of notes.
+extern const char* NOTE_NAMES[];
+/// Number of notes per octave.
+extern const int   NOTES_PER_OCTAVE;
+/// Number of cents per note.
+extern const int   CENTS_PER_NOTE;
+
+/// Integer index of note A4.
+extern const int    A4_INDEX;
+/// Frequency of note A4.
+extern const double A4_FREQ;
+
+/**
+ * @brief Normalizes angle to some given period.
+ *
+ * @param[in] angle  The angle to normalize.
+ * @param[in] period The period of the angle.
+ *
+ * @return The normalized angle.
+ */
+double normalizeAngle( double angle, double period );
+
+/**
+ * @brief Generates a name for some given frequency.
+ *
+ * @param[in]  freq The frequency.
+ * @param[out] name The name buffer.
+ * @param[out] len  Length of the name buffer.
+ */
+void nameFreq( double freq, char* name, size_t len );
+
+} // util
+
+#endif /* !__UTIL__MISC_H__INCL__ */
---
+++ src/cgt/util/Misc.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file util/Misc.cpp
+ *
+ * Console Guitar Tuner (CGT)
+ * Copyright (c) 2011 by Bloody.Rabbit
+ *
+ * @author Bloody.Rabbit
+ */
+
+#include "cgt.h"
+#include "util/Misc.h"
+
+using namespace util;
+
+/*************************************************************************/
+/* util                                                                  */
+/*************************************************************************/
+const char* util::NOTE_NAMES[] =
+{
+    "C",  "C#", "D",  "Eb",
+    "E",  "F",  "F#", "G",
+    "G#", "A",  "Bb", "B"
+};
+
+const int util::NOTES_PER_OCTAVE = 12;
+const int util::CENTS_PER_NOTE   = 100;
+
+const int    util::A4_INDEX = 4 * NOTES_PER_OCTAVE + 9;
+const double util::A4_FREQ  = 440.0;
+
+double util::normalizeAngle( double angle, double period )
+{
+    int k = ::floor( angle / period );
+    return angle -= k * period;
+}
+
+void util::nameFreq( double freq, char* name, size_t len )
+{
+    double index  = A4_INDEX + NOTES_PER_OCTAVE * ::log2( freq / A4_FREQ );
+    double cents  = CENTS_PER_NOTE * ::modf( index, &index );
+
+    int note   = index;
+    int octave = 0;
+    int k      = 0;
+
+    // Wrap cents
+    k = ( cents / CENTS_PER_NOTE ) * ( 1 + CENTS_PER_NOTE / ( 2 * ::fabs( cents ) ) );
+
+    note  += k;
+    cents -= k * CENTS_PER_NOTE;
+
+    // Wrap notes
+    k = ::floor( (double)note / NOTES_PER_OCTAVE );
+
+    octave += k;
+    note   -= k * NOTES_PER_OCTAVE;
+
+    // Print the name
+    ::snprintf( name, len, "%s%d.%+05.1lf",
+                NOTE_NAMES[ note ], octave, cents );
+}
